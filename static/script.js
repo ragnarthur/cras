576129f39ede214f52aa39109a206e7f6c24c11e
Arthur Araújo <arthuraraujo07@hotmail.com>
2024-10-10 14:07:17 -0300

    commit estilização navbar

--- static/script.js
+++ static/script.js
@@ -39,3 +39,28 @@ document.getElementById('telefone').addEventListener('input', function (e) {
     // Atualiza o valor do campo com a formatação
     e.target.value = telefone;
 });
+
+// static/script.js
+
+// Função para customizar a saudação dependendo do horário
+document.addEventListener('DOMContentLoaded', function () {
+    const saudacaoElement = document.getElementById('saudacao');
+    
+    if (saudacaoElement) {
+        const now = new Date();
+        const hours = now.getHours();
+        let saudacao = '';
+
+        if (hours < 12) {
+            saudacao = 'Bom dia';
+        } else if (hours >= 12 && hours < 18) {
+            saudacao = 'Boa tarde';
+        } else {
+            saudacao = 'Boa noite';
+        }
+
+        // Pega o nome da secretária (que já está presente na saudação padrão "Olá, nome!")
+        const nomeSecretaria = saudacaoElement.textContent.split(',')[1].trim();
+        saudacaoElement.textContent = `${saudacao}, ${nomeSecretaria}!`;
+    }
+});
